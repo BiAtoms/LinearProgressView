59ab8e190709d2b7da0ae6493c44d7f8099b41bc
Orkhan Alikhanov <orkhan.alikhanov@gmail.com>
2017-04-12 14:27:21 +0400

    Deleted CocoaPods files

--- LinearProgressViewExample/Pods/Target Support Files/LinearProgressView/LinearProgressView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LinearProgressView : NSObject
-@end
-@implementation PodsDummy_LinearProgressView
-@end
--- LinearProgressViewExample/Pods/Target Support Files/Pods-LinearProgressViewExample/Pods-LinearProgressViewExample-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_LinearProgressViewExample : NSObject
-@end
-@implementation PodsDummy_Pods_LinearProgressViewExample
-@end
